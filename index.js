562ca36f235af931b0162bdb933987b28b644c84
bamboo <minnie20000622@yahoo.co.jp>
2022-12-05 23:50:10 +0900

    タグを選んでゲーミングできるように

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-window.alert('hello world 🎉');
+const tags = ["a", "p", "button"];  // ここが選択肢になる
+var tagName = tags[0];              // 実際に選んだタグ
+
+var elements = document.getElementsByTagName(tagName);
+for (var element of elements) {
+    element.style.background = "linear-gradient(to right, Magenta, yellow, Cyan, Magenta) 0 / 200%";
+    element.style.animation = "5s example4 linear infinite";
+}

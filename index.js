e52cb752d0b3ae05b2d0bad160ca99bb2e5512c1
bamboo <minnie20000622@yahoo.co.jp>
2022-12-06 00:22:28 +0900

    色を選択肢から選べるように

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const tags = ["a", "p", "button"];  // ここが選択肢になる
-var tagName = tags[0];              // 実際に選んだタグ
+const tags = ["a", "p", "button"];  // ここがタグの選択肢になる
+var tagName = tags[1];              // 実際に選んだタグ
+
+const colors = {rainbow: "Magenta, yellow, Cyan, Magenta", pink: "deeppink, hotpink, pink, hotpink, deeppink", blue: "blue, royalblue, lightskyblue, blue"}; // ここが色の選択肢
+colorName = colors["blue"];
 
 var elements = document.getElementsByTagName(tagName);
 for (var element of elements) {
-    element.style.background = "linear-gradient(to right, Magenta, yellow, Cyan, Magenta) 0 / 200%";
+    element.style.background = "linear-gradient(to right," + colorName + ") 0 / 200%";
     element.style.animation = "5s example4 linear infinite";
 }

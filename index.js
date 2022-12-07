e75f833e3d40856edbff70e7d47f4559abdedb1e
bamboo <minnie20000622@yahoo.co.jp>
2022-12-07 21:55:03 +0900

    複数のタグを選択して色を変えられるように

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const tags = ["a", "p", "button"];  // ここが選択肢になる
-var tagName = tags[0];              // 実際に選んだタグ
+var tagName = [tags[0], tags[1]];              // 実際に選んだタグ
 
-var elements = document.getElementsByTagName(tagName);
-for (var element of elements) {
-    element.style.background = "linear-gradient(to right, Magenta, yellow, Cyan, Magenta) 0 / 200%";
-    element.style.animation = "5s example4 linear infinite";
+for (var tag of tagName) {
+    var elements = document.getElementsByTagName(tag);
+    for (var element of elements) {
+        element.style.background = "linear-gradient(to right, Magenta, yellow, Cyan, Magenta) 0 / 200%";
+        element.style.animation = "5s example4 linear infinite";
+    }
 }

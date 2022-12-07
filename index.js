6185f0f967d0c53647e4cfd6197caa9bd0cd36f5
bamboo <minnie20000622@yahoo.co.jp>
2022-12-07 22:46:15 +0900

    変数名などの変更

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const tags = ["a", "p", "button"];  // ここが選択肢になる
-var tagName = [tags[0], tags[1]];              // 実際に選んだタグ
+var tagNames = [tags[0], tags[1]];              // 実際に選んだタグ
 
-for (var tag of tagName) {
+for (var tag of tagNames) {
     var elements = document.getElementsByTagName(tag);
     for (var element of elements) {
         element.style.background = "linear-gradient(to right, Magenta, yellow, Cyan, Magenta) 0 / 200%";
-        element.style.animation = "5s example4 linear infinite";
+        element.style.animation = "5s rainbowAnimation linear infinite";
     }
 }

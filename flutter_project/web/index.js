ced1f6a0aea81c792bd9307201a756f1adf70565
bamboo <minnie20000622@yahoo.co.jp>
2022-12-09 16:35:11 +0900

    軽微なミスを修正、コメントの追加

--- flutter_project/web/index.js
+++ flutter_project/web/index.js
@@ -2,7 +2,7 @@ const tags = ["a", "p", "button"];  // ここがタグの選択肢になる
 var tagName = tags[1];              // 実際に選んだタグ
 
 const colors = {rainbow: "Magenta, yellow, Cyan, Magenta", pink: "deeppink, hotpink, pink, hotpink, deeppink", blue: "blue, royalblue, lightskyblue, blue"}; // ここが色の選択肢
-colorName = colors["blue"];
+const colorName = colors["blue"];   // 実際に選んだ色
 
 var elements = document.getElementsByTagName(tagName);
 for (var element of elements) {

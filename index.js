8882299c4ca925f8724c96826996d0d561384d26
bamboo <minnie20000622@yahoo.co.jp>
2022-12-09 16:17:23 +0900

    コンフリクトの解消

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const tags = ["a", "p", "button"];  // ここがタグの選択肢になる
-var tagName = tags[1];              // 実際に選んだタグ
+const tags = ["a", "p", "button"];  // ここが選択肢になる
+var tagNames = [tags[0], tags[1]];              // 実際に選んだタグ
 
 const colors = {rainbow: "Magenta, yellow, Cyan, Magenta", pink: "deeppink, hotpink, pink, hotpink, deeppink", blue: "blue, royalblue, lightskyblue, blue"}; // ここが色の選択肢
-colorName = colors["blue"];
+const colorName = colors["blue"];   // 実際に選んだ色
 
-var elements = document.getElementsByTagName(tagName);
-for (var element of elements) {
-    element.style.background = "linear-gradient(to right," + colorName + ") 0 / 200%";
-    element.style.animation = "5s example4 linear infinite";
+for (var tag of tagNames) {
+    var elements = document.getElementsByTagName(tag);
+    for (var element of elements) {
+        element.style.background = "linear-gradient(to right," + colorName + ") 0 / 200%";
+        element.style.animation = "5s rainbowAnimation linear infinite";
+    }
 }

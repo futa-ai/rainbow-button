5887467fddc97a73385157eb94bd48603657f2b3
bamboo <minnie20000622@yahoo.co.jp>
2022-12-10 20:38:56 +0900

    JSの中身を関数にした

--- flutter_project/web/index.js
+++ flutter_project/web/index.js
@@ -1,11 +1,26 @@
-const tags = ["a", "p", "button"];  // ここがタグの選択肢になる
-var tagName = tags[1];              // 実際に選んだタグ
+//const tags = ["a", "p", "button"];  // ここがタグの選択肢になる
+//var tagNames = tags[1];              // 実際に選んだタグ
 
-const colors = {rainbow: "Magenta, yellow, Cyan, Magenta", pink: "deeppink, hotpink, pink, hotpink, deeppink", blue: "blue, royalblue, lightskyblue, blue"}; // ここが色の選択肢
-const colorName = colors["blue"];   // 実際に選んだ色
-
-var elements = document.getElementsByTagName(tagName);
-for (var element of elements) {
-    element.style.background = "linear-gradient(to right," + colorName + ") 0 / 200%";
-    element.style.animation = "5s rainbowAnimation linear infinite";
+function changeColor(tagNames, colorName) {
+    // tagNames : list of string
+    // colorName : string
+    const colors = {rainbow: "Magenta, yellow, Cyan, Magenta", pink: "deeppink, hotpink, pink, hotpink, deeppink", blue: "blue, royalblue, lightskyblue, blue"}; // ここが色の選択肢
+    const color = colors[colorName];   // 実際に選んだ色
+    window.alert(tagNames);
+    window.alert(color);
+    setTimeout(function(){
+        console.log(document);
+        for (var tag of tagNames) {
+                console.log(document.getElementsByTagName('p').length);
+                var elements = document.getElementsByTagName(tag);
+//                window.alert(elements.length);
+                for (var element of elements) {
+//                    window.alert("good");
+                    element.style.background = "linear-gradient(to right," + color + ") 0 / 200%";
+                    element.style.animation = "5s rainbowAnimation linear infinite";
+                }
+            }
+    }, 100);
 }
+
+//changeColor(['a', "p"], "pink");

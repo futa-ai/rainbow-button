68de681416ac383ff3182f295eeb881ea8368359
bamboo <minnie20000622@yahoo.co.jp>
2022-12-11 02:49:45 +0900

    JSの関数を追加

--- flutter_project/lib/javascript.dart
+++ flutter_project/lib/javascript.dart
@@ -3,5 +3,11 @@ library index;
 
 import 'package:js/js.dart';
 
+@JS('setData')
+external void setData(List<String> tagNames, String colorName);
+
+@JS('reloadPage')
+external void reloadPage();
+
 @JS('changeColor')
-external void changeColor(List<String> tagNames, String colorName);
+external void changeColor();
